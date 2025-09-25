c0b4173d27c402e520a2a2d7b8f010caa6b8d135
hamtana <hamtana156@gmail.com>
2025-09-25 16:03:13 +1200

    changed background to white

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ export default function App() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center p-4">
-      <h1 className="text-3xl font-bold mb-6">Rate your experience</h1>
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center p-4">
+      <h1 className="text-3xl items-center font-bold mb-6">Rate your experience</h1>
 
       <StarRating onRate={handleRate} />
 
